ff3f99a293f5eabde5389a2b0a4f994e5559e1e1
Kouhei Takagi(髙木耕平) <35396762+Kouhei-Takagi@users.noreply.github.com>
2023-06-02 18:11:03 +0900

    ADD: First Commit
    2023/6/2

---
+++ NumPySample/main.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y = np.sin(x)
+
+# グラフの描画
+plt.plot(x, y)
+plt.show()
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y = np.cos(x)
+
+# グラフの描画
+plt.plot(x, y)
+plt.show()
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y = np.tan(x)
+
+# グラフの描画
+plt.plot(x, y)
+plt.show()
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y = np.exp(x)
+
+# グラフの描画
+plt.plot(x, y)
+plt.show()
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y = np.log(x)
+
+# グラフの描画
+plt.plot(x, y)
+plt.show()
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y = np.sqrt(x)
+
+# グラフの描画
+plt.plot(x, y)
+plt.show()
+
+# データの作成
+x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# グラフの描画
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos") # 破線で描画
+plt.xlabel("x") # x軸のラベル
+plt.ylabel("y") # y軸のラベル
+plt.title('sin & cos') # タイトル
+plt.legend()
+plt.show()
